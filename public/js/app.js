8d7fe363531547f71d2813ec85eeddfc33deb21d
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2020-09-09 15:22:55 -0400

    api moved to function

--- public/js/app.js
+++ public/js/app.js
@@ -27,53 +27,55 @@ $(() => {
     ///////////////////////////////////////////////////
     //////////API
     //////////////////////////////////////////////////
-    $.ajax({
-      url: 'https://kitsu.io/api/edge/anime?page[offset]=' + pageNum + '&filter[categories]='+ userInput
-    })
-    .then(
-      (data) => {
-        ///////////////////////////////////////
-        //////////CLEAR HTML
-        //////////////////////////////////////
-        $('.display').empty()
-        console.log(data);
-        ////////////////////////////////////////
-        //////LOOP THROUGH SHOWS
-        //////////////////////////////////////
-        var i;
-        for  (i = 0; i <= 10; i++) {
-          /////////////////////////////////////////
-          ///////////PRINT SHOWS
-          /////////////////////////////////////////
-          console.log(data.data[i].attributes.canonicalTitle);
-          const $title = $('<div>')
-          $title.append(data.data[i].attributes.canonicalTitle)
-          $('.display').append($title)
-          console.log(data.data[i].attributes.posterImage.small);
-          const $info = $('<div>')
-          $info.addClass('info')
-          $info.append("Title: " + data.data[i].attributes.canonicalTitle)
-          $info.append('<br>')
-          $info.append("Rating: " + data.data[i].attributes.ageRating)
-          $info.append('<br>')
-          $info.append("Plot synopsis: " + data.data[i].attributes.synopsis)
-          $info.hide()
-          $info.on('click', (event) => {
-            $(event.currentTarget).toggle();
-          })
-          $('.display').append($info)
-          const $image = $('<div>').append('<img src= "' + data.data[i].attributes.posterImage.small + '">')
-          $('.display').append($image)
-          $image.on('click', (event) => {
-            $(event.currentTarget).prev().toggle()
-          })
+    function finder() {
+      $.ajax({
+        url: 'https://kitsu.io/api/edge/anime?page[offset]=' + pageNum + '&filter[categories]='+ userInput
+      })
+      .then(
+        (data) => {
+          ///////////////////////////////////////
+          //////////CLEAR HTML
+          //////////////////////////////////////
+          $('.display').empty()
+          console.log(data);
+          ////////////////////////////////////////
+          //////LOOP THROUGH SHOWS
+          //////////////////////////////////////
+          var i;
+          for  (i = 0; i <= 10; i++) {
+            /////////////////////////////////////////
+            ///////////PRINT SHOWS
+            /////////////////////////////////////////
+            console.log(data.data[i].attributes.canonicalTitle);
+            const $title = $('<div>')
+            $title.append(data.data[i].attributes.canonicalTitle)
+            $('.display').append($title)
+            console.log(data.data[i].attributes.posterImage.small);
+            const $info = $('<div>')
+            $info.addClass('info')
+            $info.append("Title: " + data.data[i].attributes.canonicalTitle)
+            $info.append('<br>')
+            $info.append("Rating: " + data.data[i].attributes.ageRating)
+            $info.append('<br>')
+            $info.append("Plot synopsis: " + data.data[i].attributes.synopsis)
+            $info.hide()
+            $info.on('click', (event) => {
+              $(event.currentTarget).toggle();
+            })
+            $('.display').append($info)
+            const $image = $('<div>').append('<img src= "' + data.data[i].attributes.posterImage.small + '">')
+            $('.display').append($image)
+            $image.on('click', (event) => {
+              $(event.currentTarget).prev().toggle()
+            })
+          }
+
+        },
+        (error) => {
+          console.log("anime api error hit");
         }
-
-      },
-      (error) => {
-        console.log("anime api error hit");
-      }
-    )
-
+      )
+    }
+    finder();
   })
 } )
